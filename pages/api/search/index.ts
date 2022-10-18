923e92a1049569087b9af06152fa8e2272fff87d
Walter <daniwortiz003@gmail.com>
2022-10-18 18:35:49 -0300

    remove middleware

--- pages/api/search/index.ts
+++ pages/api/search/index.ts
@@ -4,8 +4,6 @@ import { searchProductsByQuery } from "controllers/search";
 
 export default methods({
     async get(req: NextApiRequest, res: NextApiResponse) {
-        console.log(req.headers);
-
         const query = req.query.q as string;
         const queryLimit = parseInt(req.query.limit as string) || 12;
         const queryOffset = parseInt(req.query.offset as string) || 0;
