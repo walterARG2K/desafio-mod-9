9d0bc4e22ab03f07a1f1520a87f089f612097f26
Walter <daniwortiz003@gmail.com>
2022-10-18 16:57:10 -0300

    try resolve cors

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,16 @@
-import type { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(req: NextRequest) {
-    if (req.method == "OPTIONS") {
-        return new Response("", {
-            status: 204,
-            headers: {
-                "Access-Control-Allow-Credentials": "true",
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-                "Access-Control-Allow-Headers": req.headers.get("Access-Control-Request-Headers"),
-                Vary: "Access-Control-Request-Headers",
-                "Content-Length": "0",
-            },
-        });
-    }
+    const headersObj = {
+        "Access-Control-Allow-Credentials": "true",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+        "Access-Control-Allow-Headers": req.headers.get("Access-Control-Request-Headers"),
+        Vary: "Access-Control-Request-Headers",
+        "Content-Length": "0",
+    };
+
+    const headersColl = Object.entries(headersObj);
+    headersColl.forEach((i) => req.headers.append(i[0], i[1]));
+    return NextResponse.next();
 }
